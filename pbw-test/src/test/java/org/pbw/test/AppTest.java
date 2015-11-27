41b81d15c7a57ef7de23bfe3ffb1d3c262cf6d13
pavittr <pavittr@9.81.60.211>
2015-11-27 16:11:38 +0000

    Initial Test Drop

---
+++ pbw-test/src/test/java/org/pbw/test/AppTest.java
@@ -0,0 +1,20 @@
+package org.pbw.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+	@Test
+    public void testApp()
+    {
+        
+    }
+}
