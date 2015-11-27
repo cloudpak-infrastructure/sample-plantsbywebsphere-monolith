80074d350f8a4e307c6cbd08a0fb7c5b2913dd70
pavittr <pavittr@9.78.64.199>
2015-11-27 23:11:48 +0000

    Improve control over deploying app to dropins

--- pbw-test/src/test/java/org/pbw/test/AppTest.java
+++ pbw-test/src/test/java/org/pbw/test/AppTest.java
@@ -1,13 +1,57 @@
 package org.pbw.test;
 
-import static org.junit.Assert.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+	
+	static String wasHome = "";
+	static String appToInstall = "";
+	
+	@BeforeClass
+	public static void setUpTest() throws IOException {
+		wasHome = System.getProperty("was.test.home");
+		appToInstall = System.getProperty("app.location");
+		
+		
+		File outLog = new File(wasHome, "logs/server1/SystemOut.log");
+		
+		BufferedReader br = new BufferedReader(new FileReader(outLog));
+		while (br.readLine() != null) {}
+		File app = new File(appToInstall);
+		
+		File wasMonitoredDir = new File(wasHome, "monitoredDeployableApps/servers/server1");
+		FileUtils.copyFileToDirectory(app, wasMonitoredDir);
+		
+		
+		String line = br.readLine();
+		while (true) {
+			if (line == null) {
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				
+				
+			} else if (line.contains("CWLDD0008I")) {break;
+			}
+
+			line = br.readLine();
+		}
+		
+		
+		br.close();
+	}
 
     /**
      * Rigourous Test :-)
@@ -15,6 +59,7 @@ public class AppTest {
 	@Test
     public void testApp()
     {
+		
         
     }
 }
